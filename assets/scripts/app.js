7b7c4a2d190f973d6909d5cd7b1bb1245c2edb8f
ozenesat <ozenesat@gmail.com>
2020-04-21 23:35:16 -0400

    User events
    All user events, sign-up/in/our and change password created with related
    html and css. Tested as well.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,24 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
+const authEvents = require('./auth/events.js')
+const surveyEvents = require('./survey/events.js')
 
 $(() => {
-  // your JS code goes here
+  // sign up for new user.
+  $('#sign-up').on('submit', authEvents.onSignUp)
+
+  // sign in for registered user.
+  $('#sign-in').on('submit', authEvents.onSignIn)
+
+  // sign out for signed-in user.
+  $('#sign-out').on('click', authEvents.onSignOut)
+
+  // change password for registered user.
+  $('#change-password').on('submit', authEvents.onChangePassword)
+
+  // clear the alerts from the page.
+  $('body').on('click', authEvents.clear)
+
+  // surveys related events.
+  // surveyEvents.addHandlers()
 })
---
+++ assets/scripts/survey/events.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const ui = require('./ui')
+const api = require('./api')
+const getFormFields = require('../../../lib/get-form-fields')
